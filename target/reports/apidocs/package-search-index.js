18d866a5ebebf62c43873ac91d673e3db2c497cb
Kamil Fulneczek <147621249+KamiloUno@users.noreply.github.com>
2026-03-20 16:44:40 +0100

    Delete target directory

--- target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.app"},{"l":"com.mycompany.controller"},{"l":"com.mycompany.model"},{"l":"com.mycompany.view"}];updateSearchResults();
